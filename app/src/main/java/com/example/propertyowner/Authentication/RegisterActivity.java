b46b2437495a1d23b818bb051c876b79e5953c8a
nephatmutugimuchiri <nephcustoms@gmail.com>
2019-05-09 16:00:33 +0300

    Authentication

---
+++ app/src/main/java/com/example/propertyowner/Authentication/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.example.propertyowner.Authentication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.propertyowner.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
