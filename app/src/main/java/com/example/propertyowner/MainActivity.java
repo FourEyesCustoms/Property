cf0528b161f3d2362af8c0bbcb8772bd74de804f
nephatmutugimuchiri <nephcustoms@gmail.com>
2019-05-15 01:28:08 +0300

    Main Activity

--- app/src/main/java/com/example/propertyowner/MainActivity.java
+++ app/src/main/java/com/example/propertyowner/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.propertyowner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.propertyowner.Authentication.LoginActivity;
+import com.example.propertyowner.Authentication.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    //widgets
+    private Button loginBtn;
+    private Button registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn=findViewById(R.id.login_btn);
+        registerBtn=findViewById(R.id.register_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
