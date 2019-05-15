d2836945e6e7760dceaf5977de527fd057b49a1f
nephatmutugimuchiri <nephcustoms@gmail.com>
2019-05-15 13:56:45 +0300

    Login and register

--- app/src/main/java/com/example/propertyowner/Authentication/LoginActivity.java
+++ app/src/main/java/com/example/propertyowner/Authentication/LoginActivity.java
@@ -2,10 +2,16 @@ package com.example.propertyowner.Authentication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.propertyowner.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+
+    //Widgets
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
